45e160a82926d71af5c98c12deb36843ede4d735
Rubens <rubserrano@gmail.com>
2019-04-15 18:25:13 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# 6_BDI_8apr_StorageII_Assignment
+
+Run container:
+
+docker run -dit -v $(pwd)/sql_scripts:/sql_scripts postgres:latest
+
+Exec Postgres Shell and create database:
+
+docker exec -it <ID_CONTAINER> psql -U postgres
+
+    psql (11.2 (Debian 11.2-1.pgdg90+1))
+    Type "help" for help.
+
+    postgres=# create database hospital;
+    CREATE DATABASE
+
+Create tables in our database:
+
+docker exec -it <ID_CONTAINER> psql -U postgres -d hospital -f /sql_scripts/script.sql
+
+Insert data in our database:
+
+docker exec -it <ID_CONTAINER> psql -U postgres -d hospital -f /sql_scripts/insert_script.sql
---
+++ sql_scripts/script.sql
@@ -0,0 +1,143 @@
+CREATE DATABASE hospital;
+
+CREATE TABLE Physician (
+  EmployeeID INTEGER PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL,
+  Position TEXT NOT NULL,
+  SSN INTEGER NOT NULL
+); 
+
+CREATE TABLE Department (
+  DepartmentID INTEGER PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL,
+  Head INTEGER NOT NULL
+    CONSTRAINT fk_Physician_EmployeeID REFERENCES Physician(EmployeeID)
+);
+
+CREATE TABLE Affiliated_With (
+  Physician INTEGER NOT NULL
+    CONSTRAINT fk_Physician_EmployeeID REFERENCES Physician(EmployeeID),
+  Department INTEGER NOT NULL
+    CONSTRAINT fk_Department_DepartmentID REFERENCES Department(DepartmentID),
+  PrimaryAffiliation BOOLEAN NOT NULL,
+  PRIMARY KEY(Physician, Department)
+);
+
+CREATE TABLE Procedure (
+  Code INTEGER PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL,
+  Cost REAL NOT NULL
+);
+
+CREATE TABLE Trained_In (
+  Physician INTEGER NOT NULL
+    CONSTRAINT fk_Physician_EmployeeID REFERENCES Physician(EmployeeID),
+  Treatment INTEGER NOT NULL
+    CONSTRAINT fk_Procedure_Code REFERENCES Procedure(Code),
+  CertificationDate timestamp without time zone NOT NULL,
+  CertificationExpires timestamp without time zone NOT NULL,
+  PRIMARY KEY(Physician, Treatment)
+);
+
+CREATE TABLE Patient (
+  SSN INTEGER PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL,
+  Address TEXT NOT NULL,
+  Phone TEXT NOT NULL,
+  InsuranceID INTEGER NOT NULL,
+  PCP INTEGER NOT NULL
+    CONSTRAINT fk_Physician_EmployeeID REFERENCES Physician(EmployeeID)
+);
+
+CREATE TABLE Nurse (
+  EmployeeID INTEGER PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL,
+  Position TEXT NOT NULL,
+  Registered BOOLEAN NOT NULL,
+  SSN INTEGER NOT NULL
+);
+
+CREATE TABLE Appointment (
+  AppointmentID INTEGER PRIMARY KEY NOT NULL,
+  Patient INTEGER NOT NULL
+    CONSTRAINT fk_Patient_SSN REFERENCES Patient(SSN),
+  PrepNurse INTEGER
+    CONSTRAINT fk_Nurse_EmployeeID REFERENCES Nurse(EmployeeID),
+  Physician INTEGER NOT NULL
+    CONSTRAINT fk_Physician_EmployeeID REFERENCES Physician(EmployeeID),
+  timeStart timestamp without time zone NOT NULL,
+  timeEnd timestamp without time zone NOT NULL,
+  ExaminationRoom TEXT NOT NULL
+);
+
+CREATE TABLE Medication (
+  Code INTEGER PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL,
+  Brand TEXT NOT NULL,
+  Description TEXT NOT NULL
+);
+
+CREATE TABLE Prescribes (
+  Physician INTEGER NOT NULL
+    CONSTRAINT fk_Physician_EmployeeID REFERENCES Physician(EmployeeID),
+  Patient INTEGER NOT NULL
+    CONSTRAINT fk_Patient_SSN REFERENCES Patient(SSN),
+  Medication INTEGER NOT NULL
+    CONSTRAINT fk_Medication_Code REFERENCES Medication(Code),
+  Date timestamp without time zone NOT NULL,
+  Appointment INTEGER
+    CONSTRAINT fk_Appointment_AppointmentID REFERENCES Appointment(AppointmentID),
+  Dose TEXT NOT NULL,
+  PRIMARY KEY(Physician, Patient, Medication, Date)
+);
+
+CREATE TABLE Block (
+  Floor INTEGER NOT NULL,
+  Code INTEGER  NOT NULL,
+  CONSTRAINT pk_Block PRIMARY KEY(Floor, Code)
+); 
+
+CREATE TABLE Room (
+  Number serial PRIMARY KEY NOT NULL,
+  Type TEXT NOT NULL,
+  Floor INTEGER NOT NULL,
+  Code INTEGER  NOT NULL,
+  FOREIGN KEY (Floor,Code) REFERENCES Block(Floor,Code),
+  Unavailable BOOLEAN NOT NULL
+);
+
+CREATE TABLE On_Call (
+  Nurse INTEGER NOT NULL
+    CONSTRAINT fk_Nurse_EmployeeID REFERENCES Nurse(EmployeeID),
+  Floor INTEGER NOT NULL,
+  Code INTEGER  NOT NULL,
+  FOREIGN KEY (Floor,Code) REFERENCES Block(Floor,Code),
+  timeStart timestamp without time zone NOT NULL,
+  timeEnd timestamp without time zone NOT NULL,
+  PRIMARY KEY(Nurse, Floor, Code, timeStart, timeEnd)
+);
+
+CREATE TABLE Stay (
+  StayID serial PRIMARY KEY NOT NULL,
+  Patient INTEGER NOT NULL
+    CONSTRAINT fk_Patient_SSN REFERENCES Patient(SSN),
+  Room INTEGER NOT NULL
+    CONSTRAINT fk_Room_Number REFERENCES Room(Number),
+  timeStart timestamp without time zone NOT NULL,
+  timeEnd timestamp without time zone NOT NULL
+);
+
+CREATE TABLE Undergoes (
+  Patient INTEGER NOT NULL
+    CONSTRAINT fk_Patient_SSN REFERENCES Patient(SSN),
+  Procedure INTEGER NOT NULL
+    CONSTRAINT fk_Procedure_Code REFERENCES Procedure(Code),
+  Stay INTEGER NOT NULL
+    CONSTRAINT fk_Stay_StayID REFERENCES Stay(StayID),
+  Date timestamp without time zone NOT NULL,
+  Physician INTEGER NOT NULL
+    CONSTRAINT fk_Physician_EmployeeID REFERENCES Physician(EmployeeID),
+  AssistingNurse INTEGER
+    CONSTRAINT fk_Nurse_EmployeeID REFERENCES Nurse(EmployeeID),
+  PRIMARY KEY(Patient, Procedure, Stay, Date)
+);
